6543f5a4ad6785c51af80b14621991ef4c2aaaa8
TeacherOp <admin@teacherop.com>
2024-09-11 17:30:51 +0530

    updated procfile for heroku

--- config.py
+++ config.py
@@ -7,8 +7,9 @@ load_dotenv()
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     DATABASE_URL = os.environ.get('DATABASE_URL')
-    SQLALCHEMY_DATABASE_URI = DATABASE_URL or 'sqlite:///app.db'  # Add a default SQLite database
+    PRODUCTION_DATABASE_URL = os.environ.get('PRODUCTION_DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = PRODUCTION_DATABASE_URL
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
+heroku git:remote -a freeaifinde
     # Add this line to print the database URI for debugging
     print(f"Database URI: {SQLALCHEMY_DATABASE_URI}")
